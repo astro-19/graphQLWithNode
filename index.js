c41f679afbe58109c00394fdff235e9b52dc9beb
Pushkar Pandey <pushkartas2418@gmail.com>
2024-03-12 17:56:38 +0530

    implemented schema, resolvers and dummy db

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+// types
+import { typeDefs } from "./schema.js";
+import db from "./_db.js"
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        authors() {
+            return db.authors
+        },
+        reviews() {
+            return db.reviews
+        }
+    }
+}
+
+// server configuration
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 1913 }
+})
+
+console.log("Server started at port " + 1913)
